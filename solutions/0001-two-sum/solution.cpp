cf7cb8df64723e1975279108488983c68485f613
Ovi-dotcom <abishankadas2004@gmail.com>
2026-08-09 06:57:15 +0000

    [LeetCode Sync] Runtime - 7 ms (41.12%), Memory - 16.1 MB (5.65%)

--- solutions/0001-two-sum/solution.cpp
+++ solutions/0001-two-sum/solution.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        vector<int>:: iterator it1;
-        vector<int>:: iterator it2;
-        for(it1 = nums.begin(); it1 != nums.end(); ++it1){
-            it2 = find(it1+1,nums.end(), target - *it1);
-            if(it2 != nums.end()){
-                result.push_back(it1 - nums.begin());
-                result.push_back(it2 - nums.begin());
-                break;
+        unordered_map<int,int> hash;
+        for(int i=0;i<nums.size();i++){
+            hash[nums[i]] = i;
+        }
+        for(int i=0;i<nums.size();i++){
+            int complement = target-nums[i];
+            if(hash.find(complement) != hash.end() && hash[complement] != i){
+                return {i,hash[complement]};
             }
         }
-        return result;
+        return {};
     }
 };
