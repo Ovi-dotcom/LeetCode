1eb8470eb3203c574437eb8c447b931c477ed501
Ovi-dotcom <abishankadas2004@gmail.com>
2026-08-11 10:23:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 63 MB (49.11%)

---
+++ solutions/0011-container-with-most-water/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0;
+        int r=height.size()-1;
+        int maxi=0;
+        while(l<r){
+            int h= min(height[l] , height[r]);
+            int w= r - l;
+            int area = h * w;
+            maxi = max(maxi , area);
+            if(height[l]<height[r]) l++;
+            else r--;
+        }
+        return maxi;
+    }
+};
