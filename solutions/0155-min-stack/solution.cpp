6526a7fe7a77938c82a3c0a2c7ee7504d3b80114
Ovi-dotcom <abishankadas2004@gmail.com>
2026-08-17 20:02:17 +0000

    [LeetCode Sync] Runtime - 58 ms (58.12%), Memory - 152.5 MB (8.02%)

---
+++ solutions/0155-min-stack/solution.cpp
@@ -0,0 +1,35 @@
+class MinStack {
+public:
+    vector<int> v;
+    vector<int> mini;
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        v.push_back(val);
+        if(mini.empty() || val <= mini.back()) mini.push_back(val);
+    }
+    
+    void pop() {
+        if(!mini.empty() && v.back() == mini.back()) mini.pop_back();
+        if(!v.empty()) v.pop_back();
+    }
+    
+    int top() {
+        return v.back();
+    }
+    
+    int getMin() {
+        return mini.back();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
